e4033cd264ec2d5c6203363636ed5183429b76fd
Artgelika <Artgelika@gmail.com>
2020-11-13 17:17:33 +0100

    Add a confusion matrix, accuracy and clear code

--- ConvNet.py
+++ ConvNet.py
@@ -41,40 +41,7 @@ y_test = pickle.load(open("y.test","rb"))
 X = X/255.0
 y = np.array(y).reshape(-1, 1)
 
-# if I want to to use the above validation, I have to reduce the dimension to twice lower
-# in this case I will take into consideration only first number from a label eg. [1,0]
-
-# reshaping y test set - [0,1] is count as 1 element, not two
-
-# creating two sets from training data
-# without dividing below it works with second "fit" 
-X_val = X[:int(len(X)*VALIDATION)]
-y_val = X[:int(len(y)*VALIDATION)]
-
-X_train = X[int(len(X)*VALIDATION):]
-y_train = X[int(len(y)*VALIDATION):]
-
-# ____shapes of matrices____
-# print("X.shape", X.shape) # (202, 100, 100, 1)
-# print("y.shape", y.shape) # (202, 2)
-
-# print("X_val.shape", X_val.shape) # (20, 100, 100, 1)
-# print("y_val.shape", y_val.shape) # (20, 2)
-
-# print("X_train.shape", X_train.shape) # (182, 100, 100, 1)
-# print("y_train.shape", y_train.shape) # (182, 2)
-
-# ____quick look at how data is looking____
-# print("X_train:", X_train[:3])
-# print("y_train:", y_train[:3])
-
-# print("X_val: ", X_val[:3])
-# print("y_val: ", y_val[:3])
-
-# print("X: ", X)
-# print("y: ", y)
-
-
+# Create a model
 model = Sequential() # Sequential - the way to build a model in Keras layer by layer
 
 model.add(Conv2D(256, (3,3), input_shape = X.shape[1:])) # 1: because we needn't to -1
@@ -107,10 +74,6 @@ model.compile(loss="binary_crossentropy", # loss=tf.keras.losses.SparseCategoric
 # print("X_val:", len(X_val)) # 20
 # print("y_val:", len(y_val)) # 20 
 
-
-# Thing below: "validation_data=(X_val, y_val)" cause an error because:  ValueError: logits and labels must have the same shape ((None, 1) vs (None, 2))
-# history = model.fit(x=X_train, y=y_train, batch_size=BATCH_SIZE, epochs=EPOCHS,  # validation_data=(X_val.T, y_val.T)
-#                     validation_data=(X_val, y_val), shuffle=True) # ? transpose or not transpose?
 model.fit(X, y, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=VALIDATION, callbacks=[tensorboard]) # batch_size= epochs = 
 # model.summary()
 
